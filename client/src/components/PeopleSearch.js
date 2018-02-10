f0f8805e8f330b7136226b9c7addc5133e90d540
akhant <a.khantelev@mail.ru>
2018-02-11 02:19:14 +0300

    Revert "Revert "add disconnected user action""
    This reverts commit 037023e81e384c6caba829f1a70bf75b22cae645.

---
+++ client/src/components/PeopleSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="search">
+      <input type="text" placeholder="search" />
+      <i className="fa fa-search" />
+    </div>
+  );
+};
